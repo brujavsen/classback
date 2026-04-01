2abadae06a122f25050e0682f16b3c1f1281a99a
Bruno Sena <brunojavsena@gmail.com>
2026-04-01 09:52:03 -0300

    feat: updated branding and PWA logos

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['classback-logo.ico', 'classback-logo.png'],
       manifest: {
         name: 'ClassBack',
         short_name: 'ClassBack',
@@ -16,14 +16,15 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/classback-logo-192px.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/classback-logo-512px.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       },
